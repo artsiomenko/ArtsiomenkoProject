088d7527f46fd1a6e4d833c495ab8acf2b127513
owner <angelichka21.02@gmail.com>
2021-12-10 19:00:28 +0300

    Add list news

--- mainpage/views.py
+++ mainpage/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    news = [
+        {
+            'description': 'South Korea’s Moon Jae-in sworn in vowing to address North'
+        },
+        {
+            'description': 'No charges over 2017 Conservative battle bus cases'
+        },
+    ]
+    return render(request, 'index.html', {'news': news})
